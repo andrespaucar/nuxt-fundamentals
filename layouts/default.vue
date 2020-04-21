a77c272243372597a2100333289f7576eb03a9d3
yoelPaucar <sjandres184@gmail.com>
2020-04-21 07:16:47 -0500

    Heroku nuxtjs

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TheNavbar/>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import TheNavbar from '@/components/TheNavbar'
+export default {
+  components:{TheNavbar}
+}
+</script>
+<style>
+/* AQUI SE APLICA EL ESTIGO GENERAL PARA LA PAGINA DEPENDIENDO DEL LAYOUT SELECIONADO */
+</style>
---
+++ store/posts.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+    all:[
+        {
+            id:'nuxt',
+            title: 'What is Nuxt?',
+            content: 'Nuxt is a progressive framework based on Vue.js to create modern web applications. It is based on Vue.js official libraries (vue, vue-router and vuex) and powerful development tools (webpack, Babel and PostCSS). Nuxts goal is to make web development powerful and performant with a great developer experience in mind.'
+        },
+        {
+            id:'balut',
+            title: 'What is Balut',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam beatae ratione dolores ut corporis quo dolor'
+        },
+        {
+            id:'how',
+            title: 'How can I fix this problem?',
+            content: 'Aliquam excepturi eos odio quos quae officia sint repudiandae molestiae, voluptates ipsa, at voluptatem?'
+        },
+    ]
+})
